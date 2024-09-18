6b360c629188332b9aaa5c198a0561bdd9f24c4a
adsauas <adsauas@gmail.com>
2024-09-18 09:43:01 +0500

    billions must consume bagels

--- src/main/java/kz/adil/student/StudentApplication.java
+++ src/main/java/kz/adil/student/StudentApplication.java
@@ -2,12 +2,22 @@ package kz.adil.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HiController{
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value= "name", defaultValue = "Guest") String name){
+		return "Hello, " + name;
+	}
+}
 
 @SpringBootApplication
-public class StudentApplication {
-
+public class StudentApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
-
 }
